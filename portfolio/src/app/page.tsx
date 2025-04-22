576f5b9969a2b82abddbf029dcc94c98e2b96242
草をこして森 <kyou0317kyou0317@gmail.com>
2025-04-22 23:00:05 +0900

    自己紹介の枠制作

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,11 +1,9 @@
+import Introduce from "@/components/Introduce";
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-8 text-center">
-      <h1 className="text-4xl font-bold mb-4">こんにちは、花子太郎です！</h1>
-      <p className="text-lg text-gray-600 max-w-xl">
-        TypeScriptとReactが大好きなフロントエンドエンジニア志望です。
-        このページでは、僕の作品やスキルを紹介しています。
-      </p>
-    </main>
+    <div>
+      <Introduce></Introduce>やほ
+    </div>
   );
 }
---
+++ portfolio/src/components/Introduce.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const Introduce = () => {
+  return (
+    <div className="w-full flex justify-center items-center py-10">
+      <div className="bg-white shadow-xl rounded-2xl p-8 w-80 text-center border">
+        <h2 className="text-2xl font-bold text-gray-800">自己紹介</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Introduce;
